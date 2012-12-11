d6e6554877dc6c9ad8174d13a90ccd24ca113e0b
e086040x <e086040x@399367ef-d520-467f-92a1-83a083ad6548>
2012-12-11 20:01:51 +0000

    comment layer.h
    git-svn-id: https://cieforge.univ-nantes.fr/svn/iatal/trunk@66 399367ef-d520-467f-92a1-83a083ad6548

--- src/model/layer.h
+++ src/model/layer.h
@@ -10,15 +10,70 @@
 
 #include "tileset.h"
 
+/*!
+  \brief A Layer is composed of a matrix of tiles and a Tileset.
+
+  Each tile of the matrix is a reference to a tile in the Tileset.
+
+  Thus, the real properties are attached to the tile in the Tileset
+  and not in the matrix itself.
+
+  There are two reasons to do so:
+  - That's how TMX stores data so it's easier to do it the same way.
+  - It takes less space in memory at the cost of another indirection.
+*/
 class Layer {
 public:
+
+  /*!
+    \brief Construct an empty Layer.
+
+  */
   Layer();
+
+  /*!
+    \brief Construct a Layer of the given size.
+
+    \param width The width of the Layer.
+    \param height The height of the Layer.
+  */
   Layer(unsigned int width, unsigned int height);
+
+  /*!
+    \brief Default destructor.
+
+  */
   virtual ~Layer();
 
+  /*!
+    \brief Returns a reference to the tile at the given position.
+
+    The reference can be used to fetch the real tile in the Tileset of
+    the Layer.
+    
+    \param x The x-coordinate or abscissa.
+    \param y The y-coordinate or ordinate.
+    \return The reference of the current tile.
+  */
   Glib::ustring getTile(unsigned int x, unsigned int y) const;
+
+  /*!
+    \brief Change the reference of the tile at the given position.
+
+    The reference has to be valid, that is, it must exist in the
+    Tileset of the Layer.
+
+    \param x The x-coordinate or abscissa.
+    \param y The y-coordinate or ordinate.
+    \param tile The new reference of the current tile.
+  */
   void setTile(unsigned int x, unsigned int y, std::string tile);
 
+  /*!
+    \brief Returns the Tileset of the Layer.
+
+    \return The Tileset of the Layer.
+  */
   Tileset & getTileset() const;
 
 private:
