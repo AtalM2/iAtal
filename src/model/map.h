1f4d31b4d07dc00bd2495039ddef1f182d6e3922
e086040x <e086040x@399367ef-d520-467f-92a1-83a083ad6548>
2012-12-11 16:25:38 +0000

    comment map.h
    git-svn-id: https://cieforge.univ-nantes.fr/svn/iatal/trunk@49 399367ef-d520-467f-92a1-83a083ad6548

--- src/model/map.h
+++ src/model/map.h
@@ -8,20 +8,42 @@
 #include "model/map-utils.h"
 #include "model/layer.h"
 
+/*!
+  \brief A Map is made of zero or more Layer.
+
+*/
 class Map
 {
   
- public:
+public:
+  
+  /*!
+    \brief Initialize an empty Map of the given size.
+
+    \param width the width of the map.
+    \param height the height of the map.
+  */
   Map(unsigned int width,
       unsigned int height);
+
+  /*!
+    \brief Returns the Layer at the given level.
+
+    \param layer An integer which denotes the level choosen.
+    \return The selected Layer.
+  */
   Layer getLayer(int layer) const;
   
   
+  /*!
+    \brief Default deconstructor.
+
+    */
   virtual ~Map();
 
- protected:
+protected:
   
- private:
+private:
   std::vector< Layer > layers_;
 };
 
