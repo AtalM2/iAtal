a603514ba1862d9f01debc14c1389c0332b3ca6e
e072376u <e072376u@399367ef-d520-467f-92a1-83a083ad6548>
2012-12-11 19:34:30 +0000

    remove deprecated stuff2
    git-svn-id: https://cieforge.univ-nantes.fr/svn/iatal/trunk@61 399367ef-d520-467f-92a1-83a083ad6548

--- src/tmx/tmx.mk
+++ src/tmx/tmx.mk
@@ -4,10 +4,10 @@ bin_PROGRAMS += \
 
 tmx_SOURCES = \
 	src/model/map.cc \
+	src/model/tileset.cc \
 	src/model/layer.cc \
 	src/tmx/main.cc \
 	src/tmx/map-loader.cc \
-	src/tmx/tileset.cc \
 	src/tmx/tmx-tileset.cc \
 	src/tmx/tinyxml2.cc \
 	src/tmx/utils.cc \
