5b071548ac8118c6697ea99a32cea7d8f6572a7a
e086040x <e086040x@399367ef-d520-467f-92a1-83a083ad6548>
2012-12-11 21:40:21 +0000

    comment Tileset
    git-svn-id: https://cieforge.univ-nantes.fr/svn/iatal/trunk@69 399367ef-d520-467f-92a1-83a083ad6548

--- src/model/tileset.h
+++ src/model/tileset.h
@@ -7,16 +7,48 @@
 
 using namespace std;
 
+/*!
+  \brief A Tileset stores the properties of each tiles in a dedicated
+  Layer.
+
+  Currently, the only property owned by a tile is its image.
+
+*/
 class Tileset {
- public:
+public:
+
+  /*!
+    \brief Create an empty Tileset.
+
+  */
   Tileset();
+  
+  /*!
+    \brief Default deconstructor.
+
+  */
   virtual ~Tileset();
+  
+  /*!
+    \brief Returns the image of the given tile.
+
+    \param tile The reference of a tile in the Tileset.
+    \return The image of the tile referenced.
+  */
   string getImage(string tile) const throw (string);
+
+  
+  /*!
+    \brief Set the image of the given tile.
+
+    \param tile The reference of a tile in the Tileset.
+    \param image The new image of the selected tile.
+  */
   void setImage(string tile, string image);
   
- private:
+private:
 
-  map<string, string> tilesList_;
+  map<string, string> tilesList_; //!< map<reference, image>
 };
 
 #endif	/* TILESET_H */
