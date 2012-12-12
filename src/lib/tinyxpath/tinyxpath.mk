f76583ff4db4c9ec81597a11522b7fcd0071d65b
e072376u <e072376u@399367ef-d520-467f-92a1-83a083ad6548>
2012-12-12 19:42:45 +0000

    tinyxpath integration
    git-svn-id: https://cieforge.univ-nantes.fr/svn/iatal/trunk@107 399367ef-d520-467f-92a1-83a083ad6548

---
+++ src/lib/tinyxpath/tinyxpath.mk
@@ -0,0 +1,18 @@
+tmx_SOURCES += \
+	src/lib/tinyxpath/htmlutil.cpp \
+	src/lib/tinyxpath/tinystr.cpp \
+	src/lib/tinyxpath/tinyxml.cpp \
+	src/lib/tinyxpath/tinyxmlerror.cpp \
+	src/lib/tinyxpath/tinyxmlparser.cpp \
+	src/lib/tinyxpath/action_store.cpp \
+	src/lib/tinyxpath/lex_util.cpp \
+	src/lib/tinyxpath/node_set.cpp \
+	src/lib/tinyxpath/tokenlist.cpp \
+	src/lib/tinyxpath/xml_util.cpp \
+	src/lib/tinyxpath/xpath_expression.cpp \
+	src/lib/tinyxpath/xpath_processor.cpp \
+	src/lib/tinyxpath/xpath_stack.cpp \
+	src/lib/tinyxpath/xpath_stream.cpp \
+	src/lib/tinyxpath/xpath_syntax.cpp \
+	src/lib/tinyxpath/xpath_static.cpp \
+	$(NULL)
