5b4331282ea7f3f43829f58a3d4b79104165e32d
e086040x <e086040x@399367ef-d520-467f-92a1-83a083ad6548>
2012-12-12 17:18:13 +0000

    use automake flags to compile test/tmx
    git-svn-id: https://cieforge.univ-nantes.fr/svn/iatal/trunk@105 399367ef-d520-467f-92a1-83a083ad6548

--- test/tmx/test.mk
+++ test/tmx/test.mk
@@ -3,12 +3,15 @@ check_PROGRAMS += tmx_tests
 tmx_tests_CXXFLAGS =	\
 	$(CPPUNIT_CFLAGS)	\
 	$(AM_CXXFLAGS)		\
-	$(gtk_CFLAGS)		\
 	$(NULL)
 
 tmx_tests_LDFLAGS =		\
 	$(CPPUNIT_LIBS)		\
-	$(gtk_LIBS)			\
+	$(AM_LDFLAGS)		\
+	$(NULL)
+
+tmx_test_CPPFLAGS =		\
+	$(AM_CPPFLAGS)		\
 	$(NULL)
 
 tmx_tests_SOURCES =				\
