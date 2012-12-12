0642e271c90cb56c4aec948734c09b873b48ebaf
e086040x <e086040x@399367ef-d520-467f-92a1-83a083ad6548>
2012-12-12 11:33:00 +0000

    m0ar test for stringExplode
    git-svn-id: https://cieforge.univ-nantes.fr/svn/iatal/trunk@92 399367ef-d520-467f-92a1-83a083ad6548

--- test/tmx/utils_test.cc
+++ test/tmx/utils_test.cc
@@ -9,13 +9,25 @@ UtilsTest::stringExplodeTest()
 {
     std::vector<string> ret;
 
-    ret = Utils::stringExplode("this;is;a;test", ";");
+    ret = Utils::stringExplode(";;;this;is;a;test;;;", ";");
     CPPUNIT_ASSERT(std::find(ret.begin(), ret.end(), "this") != ret.end());
     CPPUNIT_ASSERT(std::find(ret.begin(), ret.end(), "is") != ret.end());
     CPPUNIT_ASSERT(std::find(ret.begin(), ret.end(), "a") != ret.end());
     CPPUNIT_ASSERT(std::find(ret.begin(), ret.end(), "test") != ret.end());
+    CPPUNIT_ASSERT(ret.size() == 4);
 
     ret = Utils::stringExplode(";this;;", ";");
     CPPUNIT_ASSERT(std::find(ret.begin(), ret.end(), "this") != ret.end());
+    CPPUNIT_ASSERT(ret.size() == 1);
+
+    ret = Utils::stringExplode(";;;", ";");
+    CPPUNIT_ASSERT(ret.empty());
+
+    ret = Utils::stringExplode("", ";");
+    CPPUNIT_ASSERT(ret.empty());
+
+    ret = Utils::stringExplode("this is a test", ";");
+    CPPUNIT_ASSERT(std::find(ret.begin(), ret.end(), "this is a test") != ret.end());
+    CPPUNIT_ASSERT(ret.size() == 1);
 
 }
