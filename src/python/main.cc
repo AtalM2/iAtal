295e0eadec4e725f3551edb65eccd43e30b03dc3
e086040x <e086040x@399367ef-d520-467f-92a1-83a083ad6548>
2012-12-14 17:09:14 +0000

    fix indentation
    git-svn-id: https://cieforge.univ-nantes.fr/svn/iatal/trunk@138 399367ef-d520-467f-92a1-83a083ad6548

--- src/python/main.cc
+++ src/python/main.cc
@@ -18,75 +18,75 @@ int main(int argc, char **argv)
   Py_Initialize();
 
   try
-    {
+  {
 
-      boost::python::object main = boost::python::import("__main__"); 
-      boost::python::object py_ = main.attr("__dict__"); 
+    boost::python::object main = boost::python::import("__main__"); 
+    boost::python::object py_ = main.attr("__dict__"); 
   
-      exec_file(argv[1], py_, py_);
+    exec_file(argv[1], py_, py_);
      
-      //Joseph for memory
-      // boost::python::object func = py_["func"];
-      //int a=1, b=2;
-      //py_["a"] = boost::python::object(a);
-      //py_["b"] = object(b);
+    //Joseph for memory
+    // boost::python::object func = py_["func"];
+    //int a=1, b=2;
+    //py_["a"] = boost::python::object(a);
+    //py_["b"] = object(b);
       	
-      //boost::python::object result = func(a);
+    //boost::python::object result = func(a);
 
-      //std::string val = boost::python::extract<std::string>(result);
-      //std::cout << "iAtal dit : " << val << std::endl; 
+    //std::string val = boost::python::extract<std::string>(result);
+    //std::cout << "iAtal dit : " << val << std::endl; 
       
-      boost::python::object strat = py_["strat"];
-      boost::python::object isEnded = py_["isEnded"];
+    boost::python::object strat = py_["strat"];
+    boost::python::object isEnded = py_["isEnded"];
 
-      //Model creation. Robot in 0,0 , facing north
-      Model *aModel = new Model(0,0,std::pair<int,int>(0,1), 3, 3, 15,15);
+    //Model creation. Robot in 0,0 , facing north
+    Model *aModel = new Model(0,0,std::pair<int,int>(0,1), 3, 3, 15,15);
 
-      Matrix mat = Matrix();
+    Matrix mat = Matrix();
 
-      boost::python::object endTestRes = isEnded();
-      bool endTest = boost::python::extract<bool>(endTestRes);
-      std::cout << endTest << std::endl;
+    boost::python::object endTestRes = isEnded();
+    bool endTest = boost::python::extract<bool>(endTestRes);
+    std::cout << endTest << std::endl;
 
-      while (!endTest){
+    while (!endTest){
 	  //getElement regarde la case devant le robot et renvoie son
 	  //type
 
-	boost::shared_ptr<Model> ptr(aModel);
-        strat(boost::python::ptr(ptr));
+      boost::shared_ptr<Model> ptr(aModel);
+      strat(boost::python::ptr(ptr));
 
-	/*
-	std::string a = mat.getElement();
+      /*
+        std::string a = mat.getElement();
 	
 	
-	py_["case_avant"] = boost::python::object(a);
-	boost::python::object res = strat(a);
+        py_["case_avant"] = boost::python::object(a);
+        boost::python::object res = strat(a);
 	
-	std::string result = boost::python::extract<std::string>(res);
-	//std::cout << "result = " << result << std::endl;
+        std::string result = boost::python::extract<std::string>(res);
+        //std::cout << "result = " << result << std::endl;
 	
-	  if (result == "avance")
+        if (result == "avance")
 	    {
-	      mat.avance();
-	      std::cout << "avance" << std::endl;
+        mat.avance();
+        std::cout << "avance" << std::endl;
 	    }
-	  else
+        else
 	    {
-	      mat.turnLeft();
-	      std::cout << "tourne" << std::endl;
+        mat.turnLeft();
+        std::cout << "tourne" << std::endl;
 	    }
-	*/
+      */
 
 	  endTestRes = isEnded();
 	  endTest = boost::python::extract<bool>(endTestRes);
 	  
 
 	}
-    }
+  }
   catch (boost::python::error_already_set)
-    {
-      PyErr_Print();
-    }
+  {
+    PyErr_Print();
+  }
 
   Py_Finalize();
 
