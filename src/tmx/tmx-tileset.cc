a69e9eecd0592e2ab1b017674fd98138db595012
e086040x <e086040x@399367ef-d520-467f-92a1-83a083ad6548>
2012-12-14 14:32:20 +0000

    consistent indentation
    git-svn-id: https://cieforge.univ-nantes.fr/svn/iatal/trunk@128 399367ef-d520-467f-92a1-83a083ad6548

--- src/tmx/tmx-tileset.cc
+++ src/tmx/tmx-tileset.cc
@@ -14,67 +14,83 @@ TmxTileset::~TmxTileset() {
 
 }
 
-void TmxTileset::setSpacing(unsigned int spacing) {
+void
+TmxTileset::setSpacing(unsigned int spacing) {
   this->spacing_ = spacing;
 }
 
-unsigned int TmxTileset::getSpacing() const {
+unsigned int
+TmxTileset::getSpacing() const {
   return spacing_;
 }
 
-void TmxTileset::setTileHeight(unsigned int tileHeight) {
+void
+TmxTileset::setTileHeight(unsigned int tileHeight) {
   this->tileHeight_ = tileHeight;
 }
 
-unsigned int TmxTileset::getTileHeight() const {
+unsigned int
+TmxTileset::getTileHeight() const {
   return tileHeight_;
 }
 
-void TmxTileset::setTileWidth(unsigned int tileWidth) {
+void
+TmxTileset::setTileWidth(unsigned int tileWidth) {
   this->tileWidth_ = tileWidth;
 }
 
-unsigned int TmxTileset::getTileWidth() const {
+unsigned int
+TmxTileset::getTileWidth() const {
   return tileWidth_;
 }
 
-void TmxTileset::setHeight(unsigned int height) {
+void
+TmxTileset::setHeight(unsigned int height) {
   this->height_ = height;
 }
 
-unsigned int TmxTileset::getHeight() const {
+unsigned int
+TmxTileset::getHeight() const {
   return height_;
 }
 
-void TmxTileset::setWidth(unsigned int width) {
+void
+TmxTileset::setWidth(unsigned int width) {
   this->width_ = width;
 }
 
-unsigned int TmxTileset::getWidth() const {
+unsigned int
+TmxTileset::getWidth() const {
   return width_;
 }
 
-void TmxTileset::setImage(Glib::ustring image) {
+void
+TmxTileset::setImage(Glib::ustring image) {
   image_ = image;
 }
 
-Glib::ustring TmxTileset::getImage() const {
+Glib::ustring
+TmxTileset::getImage() const {
   return image_;
 }
 
-void TmxTileset::setFirstGid(unsigned int firstGid) {
+void
+TmxTileset::setFirstGid(unsigned int firstGid) {
   firstGid_ = firstGid;
 }
 
-unsigned int TmxTileset::getFirstGid() const {
+unsigned int
+TmxTileset::getFirstGid() const {
   return firstGid_;
 }
 
-void TmxTileset::setProperty(unsigned int id, Glib::ustring value) {
+void
+TmxTileset::setProperty(unsigned int id, Glib::ustring value) {
   propertiesList_[id] = value;
 }
 
-Glib::ustring TmxTileset::getProperty(unsigned int id) const
+Glib::ustring
+TmxTileset::getProperty(unsigned int id) const
 {
   if(!id)
     return "";
