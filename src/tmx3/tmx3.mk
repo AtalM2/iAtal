857d74353341f44b8f7b68d3c663c3254d7cbc89
e086040x <e086040x@399367ef-d520-467f-92a1-83a083ad6548>
2012-12-15 18:04:30 +0000

    start tmx3.mk
    git-svn-id: https://cieforge.univ-nantes.fr/svn/iatal/trunk@193 399367ef-d520-467f-92a1-83a083ad6548

---
+++ src/tmx3/tmx3.mk
@@ -0,0 +1,16 @@
+bin_PROGRAMS += \
+	tmx3		\
+	$(NULL)
+
+tmx3_SOURCES =					\
+	src/tmx3/image.cc			\
+	src/tmx3/layer.cc			\
+	src/tmx3/main.cc			\
+	src/tmx3/map.cc				\
+	src/tmx3/parser.cc			\
+	src/tmx3/tile.cc			\
+	src/tmx3/tileset.cc			\
+	src/tmx3/tinyxml2.cc		\
+	src/tmx3/utils.cc			\
+	$(NULL)
+
