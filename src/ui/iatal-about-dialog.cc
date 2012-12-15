36f9b5fbda7d368f9472d6b0cb233ad968679a71
e072376u <e072376u@399367ef-d520-467f-92a1-83a083ad6548>
2012-12-15 19:02:17 +0000

    about dialog
    git-svn-id: https://cieforge.univ-nantes.fr/svn/iatal/trunk@205 399367ef-d520-467f-92a1-83a083ad6548

---
+++ src/ui/iatal-about-dialog.cc
@@ -0,0 +1,12 @@
+// -*- c-basic-offset: 2; -*-
+#include "iatal-about-dialog.h"
+
+IAtalAboutDialog::IAtalAboutDialog()
+{
+  
+}
+
+IAtalAboutDialog::~IAtalAboutDialog()
+{
+  
+}
---
+++ src/ui/iatal-about-dialog.h
@@ -0,0 +1,16 @@
+// -*- c-basic-offset: 2; -*-
+#ifndef IATAL_ABOUT_DIALOG_H
+#define IATAL_ABOUT_DIALOG_H
+
+#include <gtkmm.h>
+
+class IAtalAboutDialog : public Gtk::AboutDialog
+{
+
+ public:
+  IAtalAboutDialog();
+  virtual ~IAtalAboutDialog();
+  
+};
+
+#endif // IATAL_ABOUT_DIALOG_H
