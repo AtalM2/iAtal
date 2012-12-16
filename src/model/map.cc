b81eff966c5748573dc5573c43d856e962161b12
e072376u <e072376u@399367ef-d520-467f-92a1-83a083ad6548>
2012-12-16 18:07:43 +0000

    merge
    git-svn-id: https://cieforge.univ-nantes.fr/svn/iatal/trunk@291 399367ef-d520-467f-92a1-83a083ad6548

--- src/model/map.cc
+++ src/model/map.cc
@@ -11,25 +11,6 @@ BOOST_PYTHON_MODULE(elements)
     .def("turnLeft", &Map::robotTurnLeft);
 }
 
-Map::Map(unsigned int posXRobot,
-	 unsigned int posYRobot,
-	 std::pair<int,int> direction,
-	 unsigned int width,
-         unsigned int height,
-	 unsigned int tileWidth,
-         unsigned int tileHeight)
-  : height(height),
-    width(width),
-    tileHeight(tileHeight),
-    tileWidth(tileWidth),
-    posXRobot(posXRobot),
-    posYRobot(posYRobot),
-    direction(direction),
-    layers_(Layer::Level::LastLevel + 1, Layer(width, height))
-{
-
-}
-
 Map::Map(unsigned int width,
          unsigned int height,
 	 unsigned int tileWidth,
@@ -49,7 +30,6 @@ Map::Map()
     tileWidth(10)
 {
 }
-
 
 std::string Map::getItem(Layer::Level level, unsigned int range)
 {
@@ -108,7 +88,8 @@ void Map::robotTurnLeft()
 
 void Map::goForward()
 {
-  if ((int)(direction.first + posXRobot) >= 0 && (int)(direction.second + posYRobot) >= 0)
+  if (direction.first + static_cast<int>(posXRobot) >= 0
+      && direction.second + static_cast<int>(posYRobot) >= 0)
   {
     unsigned int moveX = posXRobot + direction.first;
     unsigned int moveY = posYRobot + direction.second;
@@ -127,9 +108,8 @@ Map::~Map()
 
 }
 
-Layer&
+Layer &
 Map::getLayer(Layer::Level level)
 {
   return layers_.at(static_cast<unsigned int>(level));
 }
-
