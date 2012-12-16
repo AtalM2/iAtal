593576afd2b614b0448e550dae9938aa0bbcd895
e072376u <e072376u@399367ef-d520-467f-92a1-83a083ad6548>
2012-12-16 02:37:23 +0000

    start doc
    git-svn-id: https://cieforge.univ-nantes.fr/svn/iatal/trunk@229 399367ef-d520-467f-92a1-83a083ad6548

--- src/ui/iatal-about-dialog.h
+++ src/ui/iatal-about-dialog.h
@@ -3,17 +3,41 @@
 #define IATAL_ABOUT_DIALOG_H
 
 #include <gtkmm.h>
-
 class IAtalAboutDialog : public Gtk::AboutDialog
 {
 
  public:
+  
+  /**
+   * Constructor.
+   * 
+   * Sets all the interesting values to build the about dialog.
+   */
   IAtalAboutDialog();
+
+  /**
+   * Destructor.
+   */
   virtual ~IAtalAboutDialog();
   
  private:
+
+  /**
+   * vector containing the name of the persons that have been involved
+   * in the programming of iAtal.
+   */
   std::vector< Glib::ustring > authors_,
+
+  /**
+   * vector containing the name of the persons that have been involved
+   * in the documentation realization.
+   */
     documenters_,
+
+  /**
+   * vector containing the name of the persons that have been involved
+   * in the artworks realization.
+   */
     artists_;
 
 };
