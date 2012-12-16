bbd3997e8817e16b95b38b40a4ce246dcfae420f
e10b347x <e10b347x@399367ef-d520-467f-92a1-83a083ad6548>
2012-12-16 23:05:57 +0000

    redecoupe des modules py
    git-svn-id: https://cieforge.univ-nantes.fr/svn/iatal/trunk@324 399367ef-d520-467f-92a1-83a083ad6548

--- src/python/init_strat.py
+++ src/python/init_strat.py
@@ -16,9 +16,8 @@ def init(aMap):
 	global map_ 
 	map_ = aMap
 
-def robot_init():
-	map_.setDirection(0,1)
-	map_.setPosition(0,0)
+def loadMap()
+	return map_
 
 #movement functions
 def walk():
@@ -31,50 +30,6 @@ def turnRight():
 	map_.turnRight()
 
 
-#sensors
-	
-def undergroundSensor():
-	return sensor(map_,enums.Level.Underground, 1).activate()
 
-def groundSensor():
-	return sensor(map_,enums.Level.Ground, 1).activate()
-
-def objectSensor():
-	return sensor(map_,enums.Level.Object, 1).activate()
-
-#def airSensor(): 
-#	return sensor(map_,enums.Level.Air, 1).activate()
-
-#actuators
-
-	#underground
-def greenActuator():
-	return actuator(map_,enums.Level.Underground, 1, "type:vert").activate()
-	
-def	brownActuator():
-	return actuator(map_,enums.Level.Underground, 1, "type:marron").activate()
-
-def	whiteActuator():
-	return actuator(map_,enums.Level.Underground, 1, "type:blanc").activate()
-
-def	blueActuator():
-	return actuator(map_,enums.Level.Underground, 1, "type:bleu").activate()
-
-	#ground
-def	herbeActuator():
-	return actuator(map_,enums.Level.Ground, 1, "type:herbe").activate()
-
-def	eauActuator():
-	return actuator(map_,enums.Level.Ground, 1, "type:eau").activate()
-
-	#object
-def	fleurActuator():
-	return actuator(map_,enums.Level.Object, 1, "type:fleur").activate()
-
-def	champiActuator():
-	return actuator(map_,enums.Level.Object, 1, "couleur:rouge").activate()
 	
 
-#sert à savoir si la strat est finie ou non
-def isEnded():
-	return ended;
--- src/python/test.py
+++ src/python/test.py
@@ -13,9 +13,15 @@ compteur = 0
 #map_ = False
 ended = False
 
-#sert à savoir si la strat est finie ou non
-def isEnded():
-	return ended;	
+#map loading
+global map_ 
+	map_ = loadMap()
+
+#position and direction initialization
+def robot_init():
+	map_.setDirection(0,1)
+	map_.setPosition(0,0)
+
 
 #stratégie python
 def strat():
@@ -35,3 +41,68 @@ def strat():
 
 
 
+
+#sensors
+
+	#one tile ahead	
+def undergroundSensorOnSelf():
+	return sensor(map_,enums.Level.Underground, 0).activate()
+
+def groundSensorOnSelf():
+	return sensor(map_,enums.Level.Ground, 0).activate()
+
+def objectSensorOnSelf():
+	return sensor(map_,enums.Level.Object, 0).activate()
+
+def airSensorOnSelf(): 
+	return sensor(map_,enums.Level.Air, 0).activate()
+
+	#on self
+def undergroundSensor():
+	return sensor(map_,enums.Level.Underground, 1).activate()
+
+def groundSensor():
+	return sensor(map_,enums.Level.Ground, 1).activate()
+
+def objectSensor():
+	return sensor(map_,enums.Level.Object, 1).activate()
+
+def airSensor(): 
+	return sensor(map_,enums.Level.Air, 1).activate()
+
+
+
+
+#actuators
+
+	#underground
+def greenActuator():
+	return actuator(map_,enums.Level.Underground, 1, "type:vert").activate()
+	
+def	brownActuator():
+	return actuator(map_,enums.Level.Underground, 1, "type:marron").activate()
+
+def	whiteActuator():
+	return actuator(map_,enums.Level.Underground, 1, "type:blanc").activate()
+
+def	blueActuator():
+	return actuator(map_,enums.Level.Underground, 1, "type:bleu").activate()
+
+	#ground
+def	herbeActuator():
+	return actuator(map_,enums.Level.Ground, 1, "type:herbe").activate()
+
+def	eauActuator():
+	return actuator(map_,enums.Level.Ground, 1, "type:eau").activate()
+
+	#object
+def	fleurActuator():
+	return actuator(map_,enums.Level.Object, 1, "type:fleur").activate()
+
+def	champiActuator():
+	return actuator(map_,enums.Level.Object, 1, "couleur:rouge").activate()
+
+
+#sert à savoir si la strat est finie ou non
+def isEnded():
+	return ended;	
