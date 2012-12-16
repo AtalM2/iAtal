d59528ef2a6408300d919a99ee0b9da7938387ff
e086040x <e086040x@399367ef-d520-467f-92a1-83a083ad6548>
2012-12-16 21:51:07 +0000

    pretty format
    git-svn-id: https://cieforge.univ-nantes.fr/svn/iatal/trunk@312 399367ef-d520-467f-92a1-83a083ad6548

--- src/ui/map-area.cc
+++ src/ui/map-area.cc
@@ -27,7 +27,7 @@ MapArea::setMap(const std::shared_ptr< Map > & map)
   map_ = map;
   if(map.get())
     set_size_request(map->width * map->tileWidth,
-		     map->height * map->tileHeight);
+                     map->height * map->tileHeight);
 }
 
 MapArea::~MapArea()
@@ -41,49 +41,50 @@ bool MapArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
     map_->getLayer(Layer::Level::Ground),
     map_->getLayer(Layer::Level::Object),
   };
+  
   std::for_each(
     layers.begin(),
     layers.end(),
-    [this, & cr] (const Layer & layer)
-    {
+    [this, & cr] (const Layer & layer) {
       Tileset tileset = layer.getTileset();
-      for(unsigned int x = 0; x < map_->width; x++)
-	{
-	  for(unsigned int y = 0; y < map_->height; y++)
-	    {
+      
+      for(unsigned int x = 0; x < map_->width; x++) {
+        for(unsigned int y = 0; y < map_->height; y++) {
+          
 	      Glib::ustring id = layer.getTile(x, y);
-	      if(id != "")
-		{
-		  Glib::RefPtr< const Gdk::Pixbuf > image;
-		  try
-		    {
+          
+	      if(id != "") {
+            Glib::RefPtr< const Gdk::Pixbuf > image;
+            try {
 		      image = tileset.getImage(id);
-		    }
-		  catch(const std::exception & e)
-		    {
+		    } catch(const std::exception & e) {
 		      std::ostringstream oss;
+              
 		      oss << "A problem occured while "
-				<< "drawing the map. It seems "
-				<< "we're trying to draw a non "
-				<< "existing tile:"
-				<< std::endl
-				<< e.what()
-				<< std::endl;
+                  << "drawing the map. It seems "
+                  << "we're trying to draw a non "
+                  << "existing tile:"
+                  << std::endl
+                  << e.what()
+                  << std::endl;
+              
 		      AppController::displayWarning(
-			"Fatal Error while drawing map",
-			oss.str());
+                "Fatal Error while drawing map",
+                oss.str());
+              
 		      exit(1);
 		    }
-		  Gdk::Cairo::set_source_pixbuf(
-		    cr,
-		    Glib::RefPtr< Gdk::Pixbuf >::cast_const(image),
-		    x * map_->tileWidth,
-		    y * map_->tileHeight);
-		  cr->paint();
-		}
+            Gdk::Cairo::set_source_pixbuf(
+              cr,
+              Glib::RefPtr< Gdk::Pixbuf >::cast_const(image),
+              x * map_->tileWidth,
+              y * map_->tileHeight);
+            
+            cr->paint();
+          }
 	    }
-	}
+      }
     }
-  );
+    );
   return true;
 }
