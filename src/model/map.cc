81371546f8f774462bbe3124146ae40f35e70ccb
e072376u <e072376u@399367ef-d520-467f-92a1-83a083ad6548>
2012-12-16 18:07:47 +0000

    Revert "merge"
    This reverts commit 83d325111b0071b669dbf1d4e6f641942561c1e3.
    
    git-svn-id: https://cieforge.univ-nantes.fr/svn/iatal/trunk@292 399367ef-d520-467f-92a1-83a083ad6548

--- src/model/map.cc
+++ src/model/map.cc
@@ -11,6 +11,25 @@ BOOST_PYTHON_MODULE(elements)
     .def("turnLeft", &Map::robotTurnLeft);
 }
 
+Map::Map(unsigned int posXRobot,
+	 unsigned int posYRobot,
+	 std::pair<int,int> direction,
+	 unsigned int width,
+         unsigned int height,
+	 unsigned int tileWidth,
+         unsigned int tileHeight)
+  : height(height),
+    width(width),
+    tileHeight(tileHeight),
+    tileWidth(tileWidth),
+    posXRobot(posXRobot),
+    posYRobot(posYRobot),
+    direction(direction),
+    layers_(Layer::Level::LastLevel + 1, Layer(width, height))
+{
+
+}
+
 Map::Map(unsigned int width,
          unsigned int height,
 	 unsigned int tileWidth,
@@ -30,6 +49,7 @@ Map::Map()
     tileWidth(10)
 {
 }
+
 
 std::string Map::getItem(Layer::Level level, unsigned int range)
 {
@@ -88,8 +108,7 @@ void Map::robotTurnLeft()
 
 void Map::goForward()
 {
-  if (direction.first + static_cast<int>(posXRobot) >= 0
-      && direction.second + static_cast<int>(posYRobot) >= 0)
+  if ((int)(direction.first + posXRobot) >= 0 && (int)(direction.second + posYRobot) >= 0)
   {
     unsigned int moveX = posXRobot + direction.first;
     unsigned int moveY = posYRobot + direction.second;
@@ -108,8 +127,9 @@ Map::~Map()
 
 }
 
-Layer &
+Layer&
 Map::getLayer(Layer::Level level)
 {
   return layers_.at(static_cast<unsigned int>(level));
 }
+
