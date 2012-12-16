d891fdec0ab1fd0eb28a4e5c3b78a93683476b5c
e086040x <e086040x@399367ef-d520-467f-92a1-83a083ad6548>
2012-12-16 15:48:31 +0000

    broken python to bullet proof the ui
    git-svn-id: https://cieforge.univ-nantes.fr/svn/iatal/trunk@265 399367ef-d520-467f-92a1-83a083ad6548

---
+++ src/python/test_broken.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import elements
+import enums
+
+#globals
+ended = False
+compteur = 0
+map_ = False
+
+def init(aMap):
+	global map_ 
+	map_ = aMap
+
+def robot_init():
+	global undergroundSensor 
+	undergroundSensor = sensor(enums.Level.Underground, 1)
+	global greenActuator 
+	greenActuator = actuator(enums.Level.Underground, 1, "bibi")
+
+#stratégie python
+def strat():
+	print(undergroundSensor.activate())
+	greenActuator.activate()
+	print(undergroundSensor.activate())
+	global ended
+	ended = True
+
+#sert à savoir si la strat est finie ou non
+def isEnded():
+	return ended;
+
+#Defines a sensor.
+class sensor:
+	def __init__(self,level_, range_):
+		self.map_ = map_
+		self.level_ = level_
+		self.range_ = range_
+	def activate(self):
+		return map_.getItem(self.level_ , self.range_)
+
+#defines an actuator
+class actuator:
+	def __init__(self, level_, range_,newContent_):
+		self.map_ = map_
+		self.level_ = level_
+		self.range_ = range_
+		self.newContent_ = newContent_
+	
+	def activate(self):
+		self.map_.setItem(self.level_, self.range_, self.newContent_)
