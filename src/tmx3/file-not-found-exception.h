c961964524be7a2bbaf4e8533c44ce4678455b46
e072376u <e072376u@399367ef-d520-467f-92a1-83a083ad6548>
2012-12-16 18:07:22 +0000

    add tmx namespace to exceptions 3/4
    git-svn-id: https://cieforge.univ-nantes.fr/svn/iatal/trunk@287 399367ef-d520-467f-92a1-83a083ad6548

--- src/tmx3/file-not-found-exception.h
+++ src/tmx3/file-not-found-exception.h
@@ -3,15 +3,17 @@
 
 #include "exception.h"
 
-class FileNotFoundException : public Exception {
-public:
+namespace tmx
+{
+  class FileNotFoundException : public Exception {
+  public:
 
-	FileNotFoundException(const std::string & complement) throw ()
-        : Exception("FileNotFound: " + complement) {
-	}
-
-	virtual ~FileNotFoundException() throw () {
-	}
-};
+    FileNotFoundException(const std::string & complement)
+      throw ()
+      : Exception("FileNotFound: " + complement) { }
+  
+    virtual ~FileNotFoundException() throw () { }
+  };
+}
 
 #endif // FILE_NOT_FOUND_EXCEPTION_H
