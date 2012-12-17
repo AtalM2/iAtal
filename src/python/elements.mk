22bbf528ec51d644220d1992d47cf4fed086984a
e073332j <e073332j@399367ef-d520-467f-92a1-83a083ad6548>
2012-12-17 22:40:17 +0000

    modif de elements.mk pour que ça compile. Test from scratch OK
    git-svn-id: https://cieforge.univ-nantes.fr/svn/iatal/trunk@414 399367ef-d520-467f-92a1-83a083ad6548

--- src/python/elements.mk
+++ src/python/elements.mk
@@ -26,30 +26,4 @@ elements_la_SOURCES =			\
 	src/model/tileset.cc		\
 	$(NULL)
 
-lib_LTLIBRARIES += \
-	python_hello.la \
-	$(NULL)
 
-python_hello_la_SOURCES =		\
-	src/python/python_hello.cc	\
-	$(NULL)
-
-python_hello_la_LDFLAGS =		\
-	$(AM_LDFLAGS)				\
-	-avoid-version				\
-	-module						\
-	$(NULL)
-
-bin_PROGRAMS += cpp_hello
-cpp_hello_SOURCES =	src/python/cpp_hello.cc
-
-bin_PROGRAMS += cpp_hello_python
-
-cpp_hello_python_SOURCES = \
-	src/python/cpp_hello_python.cc \
-	$(NULL)
-
-cpp_hello_python_LDFLAGS =		\
-	$(AM_LDFLAGS)				\
-	src/python/python_hello.cc	\
-	$(NULL)
