223f75407d45ad84187eb8688e46566cde9b2ba2
e073332j <e073332j@399367ef-d520-467f-92a1-83a083ad6548>
2012-12-17 22:27:35 +0000

    Nettoyage du dossier python
    git-svn-id: https://cieforge.univ-nantes.fr/svn/iatal/trunk@410 399367ef-d520-467f-92a1-83a083ad6548

--- src/python/cpp_hello.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-def greet():
-    return "Hello World!"
-
-print(greet())
--- src/python/cpp_hello_python.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import sys
-sys.path.append('.libs')
-import python_hello
-world = python_hello.World()
-world.set('Hello World!')
-print(world.greet())
