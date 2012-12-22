a13383fc35d3c1dba6297f425c458c3478046a04
Grégoire Jadi <gregoire.jadi@gmail.com>
2012-12-22 18:02:44 +0100

    add license + emacs headers in sources

--- src/ui/python-chooser-dialog.cc
+++ src/ui/python-chooser-dialog.cc
@@ -1,4 +1,22 @@
 // -*- c-basic-offset: 2; -*-
+
+//  Copyright (C) 2012
+
+// This file is part of iAtal.
+
+// iAtal is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// iAtal is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with iAtal.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "ui/python-chooser-dialog.h"
 
 PythonChooserDialog::PythonChooserDialog()
